2749049751a154e45a5b1833b8c9201dfa611a17
Tim <13Tim37@gmail.com>
2016-02-15 11:12:26 +0000

    Added string functions for printing.
    Allows shape classes to be printed.

--- a_star_implemented/obj.py
+++ a_star_implemented/obj.py
@@ -30,6 +30,9 @@ class Triangle(Object):
 
     def print_obj(self):
         return(pygame.draw.polygon(self.screen,self.colour,((self.x1,self.y1),(self.x2,self.y2),(self.x3,self.y3))))
+    def __str__(self):
+        return(__class__.__name__ + " with a price of " + str(self.price))
+    
 class Quadrilater(Object):
     def __init__(self,x1,y1,price,dimension,screen,colour):
         Object.__init__(self,x1,y1,screen,colour)
@@ -43,6 +46,9 @@ class Square(Quadrilater):
         self.weight=dimension
     def print_obj(self):
         return(pygame.draw.rect(self.screen,self.colour,(self.x1,self.y1,self.heigth,self.weight)))
+    def __str__(self):
+        return(__class__.__name__ + " with a price of " + str(self.price))
+    
 class Rectangle(Quadrilater):
     def __init__(self,x1,y1,price,dimension,screen,colour):
         Quadrilater.__init__(self,x1,y1,price,dimension,screen,colour)
@@ -50,6 +56,9 @@ class Rectangle(Quadrilater):
         self.weight=dimension
     def print_obj(self):
         return(pygame.draw.rect(self.screen,self.colour,(self.x1,self.y1,self.heigth,self.weight)))
+    def __str__(self):
+        return(__class__.__name__ + " with a price of " + str(self.price))
+    
 class Circle(Object):
     def __init__(self,x1,y1,dimension,screen,colour):
         Object.__init__(self,x1,y1,screen,colour)
