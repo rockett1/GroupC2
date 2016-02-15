58ab56a7907f6a59977d390a20723e5a66d46ef0
Tim <13Tim37@gmail.com>
2016-02-15 11:07:53 +0000

    Revert "obj.py_changed"
    This reverts commit 1cfde07ce38f33d9cb4e453508060a41593f18af.

--- a_star_implemented/obj.py
+++ a_star_implemented/obj.py
@@ -15,10 +15,9 @@ class Object:
 
         self.colour=colour
         self.screen=screen
+        
     def print_obj(self):
         pass
-	def get_coord(self):
-		return((self.x1,self.x2))
 class Triangle(Object):
     def __init__(self,x1,y1,price,dimension,screen,colour):
         Object.__init__(self,x1,y1,screen,colour)
@@ -30,12 +29,17 @@ class Triangle(Object):
 
     def print_obj(self):
         return(pygame.draw.polygon(self.screen,self.colour,((self.x1,self.y1),(self.x2,self.y2),(self.x3,self.y3))))
+
+    def __str__(self):
+        return(__class__.__name__ + " with a price of " + str(self.price))
+               
 class Quadrilater(Object):
     def __init__(self,x1,y1,price,dimension,screen,colour):
         Object.__init__(self,x1,y1,screen,colour)
         self.price=price
     def print_obj(self):
         pass
+    
 class Square(Quadrilater):
     def __init__(self,x1,y1,price,dimension,screen,colour):
         Quadrilater.__init__(self,x1,y1,price,dimension,screen,colour)
@@ -43,6 +47,9 @@ class Square(Quadrilater):
         self.weight=dimension
     def print_obj(self):
         return(pygame.draw.rect(self.screen,self.colour,(self.x1,self.y1,self.heigth,self.weight)))
+    def __str__(self):
+        return(__class__.__name__ + " with a price of " + str(self.price))
+               
 class Rectangle(Quadrilater):
     def __init__(self,x1,y1,price,dimension,screen,colour):
         Quadrilater.__init__(self,x1,y1,price,dimension,screen,colour)
@@ -50,6 +57,9 @@ class Rectangle(Quadrilater):
         self.weight=dimension
     def print_obj(self):
         return(pygame.draw.rect(self.screen,self.colour,(self.x1,self.y1,self.heigth,self.weight)))
+    def __str__(self):
+        return(__class__.__name__ + " with a price of " + str(self.price))
+               
 class Circle(Object):
     def __init__(self,x1,y1,dimension,screen,colour):
         Object.__init__(self,x1,y1,screen,colour)
